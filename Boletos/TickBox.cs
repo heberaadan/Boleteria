617f325a85d21f80714d24d49e5a0204170124ef
Heber Adan <heberadan.pilaresgarcia@gmail.com>
2024-09-14 20:52:22 -0600

    Se agrega el diseño del inicio de sesion

---
+++ Boletos/TickBox.cs
@@ -0,0 +1,20 @@
+namespace Boletos
+{
+    public partial class TickBox : Form
+    {
+        public TickBox()
+        {
+            InitializeComponent();
+        }
+
+        private void sing_in_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TickBox_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
